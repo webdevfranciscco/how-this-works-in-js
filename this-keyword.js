3a1c03681f6c6a85334c47fee62cf2d42ff301b5
wbdevfranciscco <webdevfranciscco@gmail.com>
2025-06-24 13:37:59 -0600

    fix the comparisons by adding "typeof"

--- this-keyword.js
+++ this-keyword.js
@@ -1,17 +1,17 @@
-"use strict";
+'use strict';
 
 // ( 1 )
 // "this" in the global scope
 
-const globalScopeOutput = document.querySelector("#globalScopeOutput");
+const globalScopeOutput = document.querySelector('#globalScopeOutput');
 
-this === "undefined"
+typeof this === 'undefined'
   ? (globalScopeOutput.textContent = `The value of "this" in the global scope is: ${typeof this}`)
   : (globalScopeOutput.textContent = `The value of "this" in the global scope is: ${this}`);
 // 'undefined' could have been used instead of "typeof this", but somehow the latter felt better (as confirmation?)
 
 console.log(
-  this === "undefined"
+  typeof this === 'undefined'
     ? `1. The value of "this" in the global scope is: ${typeof this}`
     : `1. The value of "this" in the global scope is: ${this}`
 );
